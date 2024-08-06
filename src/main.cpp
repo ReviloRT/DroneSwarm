d1be729feb2643cc87e82bb5a4ae3a3ae5ba50b6
ReviloRT <opvannoort@gmail.com>
2024-08-06 23:09:35 +1200

    RGB_led working

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,31 @@
 #include <Arduino.h>
 
 #define PIN_LED GPIO_NUM_2
+#define PIN_RGB_R GPIO_NUM_8
+#define PIN_RGB_G GPIO_NUM_9
+#define PIN_RGB_B GPIO_NUM_10
 
 void blink(uint8_t pin, uint32_t millis);
+void set_RGB_LED(bool R_on, bool G_on, bool B_on);
 
 void setup() {
   pinMode(PIN_LED,OUTPUT);
+  pinMode(PIN_RGB_R,OUTPUT);
+  pinMode(PIN_RGB_G,OUTPUT);
+  pinMode(PIN_RGB_B,OUTPUT);
+  set_RGB_LED(1,1,1);
+  delay(1000);
 }
 
 void loop() {
-  blink(PIN_LED,250);
+  blink(PIN_LED,100);
+  
+  set_RGB_LED(1,0,0);
+  delay(200);
+  set_RGB_LED(0,1,0);
+  delay(200);
+  set_RGB_LED(0,0,1);
+  delay(200);
 }
 
 void blink(uint8_t pin, uint32_t millis) {
@@ -17,4 +33,10 @@ void blink(uint8_t pin, uint32_t millis) {
   delay(millis);
   digitalWrite(pin,LOW);
   delay(millis);
+}
+
+void set_RGB_LED(bool R_on, bool G_on, bool B_on) {
+  digitalWrite(PIN_RGB_R,R_on);
+  digitalWrite(PIN_RGB_G,G_on);
+  digitalWrite(PIN_RGB_B,B_on);
 }
