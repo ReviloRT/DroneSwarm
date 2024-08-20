f11403c54d880d0993b8f8132b9f4b2766fbde8e
ReviloRT <opvannoort@gmail.com>
2024-08-20 16:13:07 +1200

    Tidied Main

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,21 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include "imu.hpp"
-
 #include "led.hpp"
-#include "rotors.hpp"
-
-Rotors rotors;
-
-
-#define PIN_LED GPIO_NUM_2
 
 BMI323 imu;
 
-void blink(uint8_t pin, uint32_t millis);
-
 void setup() {
   init_leds();
-
-  delay(1000);
-  rotors.on(128);
   Serial.begin(115200);
 
   digitalWrite(PIN_LED,HIGH);
+  set_RGB_LED(1,1,1);
 
   imu.init();
 }
 
 void loop() {
   imu.update();
-}
-
-void blink(uint8_t pin, uint32_t millis) {
-  digitalWrite(pin,HIGH);
-  delay(millis);
-  digitalWrite(pin,LOW);
-  delay(millis);
+  set_RGB_LED(0,1,0);
 }
