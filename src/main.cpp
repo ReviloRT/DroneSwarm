e951110f34a23910982f9e159468cd289ecbd530
ReviloRT <opvannoort@gmail.com>
2024-08-20 16:12:01 +1200

    Basic implimentation, short in PCB

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,80 @@
 #include <Arduino.h>
+#include <Wire.h>
 
 #include "led.hpp"
 #include "rotors.hpp"
 
 Rotors rotors;
 
+
+#define PIN_LED GPIO_NUM_2
+#define BMI323_ADDR 0x69
+#define PIN_SDA GPIO_NUM_36
+#define PIN_SCL GPIO_NUM_35
+
+void blink(uint8_t pin, uint32_t millis);
+void read_data();
+
 void setup() {
   init_leds();
-  rotors.init();
 
   delay(1000);
   rotors.on(128);
+  Serial.begin(115200);
+  Wire.begin(PIN_SDA,PIN_SCL,400000);
+}
+
+// void loop() {
+//   blink(PIN_LED,250);
+//   read_data();
+// }
+
+void blink(uint8_t pin, uint32_t millis) {
+  digitalWrite(pin,HIGH);
+  delay(millis);
+  digitalWrite(pin,LOW);
+  delay(millis);
+}
+
+void read_data() {
+  Wire.beginTransmission(BMI323_ADDR);
+  // Wire.write(0x03);
+  int error = Wire.endTransmission();
+  Serial.printf("Error %d\n",error);
+  // int n_rec = Wire.requestFrom(BMI323_ADDR, 2);
+  
+  // if (n_rec < 2) return;
+
+  // uint16_t rec[1] = {0};
+  // for (size_t i = 0; i < 1; i++) {
+  //   uint8_t b1 = Wire.read();
+  //   uint8_t b2 = Wire.read();
+  //   rec[i] = (b2 << 8) | b1;
+  // }   
+  
+  // Serial.printf("%d, %d, %d ,%d, %d, %d\n",rec[0],rec[1],rec[2],rec[3],rec[4],rec[5]);
+
 }
 
 
 void loop() {
+  byte error, address;
+  int nDevices = 0;
 
-  set_RGB_LED(1,1,0);
-  for (size_t i = 0; i < 255; i++) {
-    rotors.power(MOTOR_1,i);
-    rotors.power(MOTOR_2,i);
-    rotors.power(MOTOR_3,i);
-    rotors.power(MOTOR_4,i);
-    delay(10);
+  delay(5000);
+
+  Serial.println("Scanning for I2C devices ...");
+  for(address = 0x68; address < 0x6A; address++){
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+    if (error == 0){
+      Serial.printf("I2C device found at address 0x%02X\n", address);
+      nDevices++;
+    } else if(error != 2){
+      Serial.printf("Error %d at address 0x%02X\n", error, address);
+    }
   }
-  set_RGB_LED(0,1,0);
-  delay(1000);
-
-  set_RGB_LED(1,1,0);
-  for (size_t i = 0; i < 255; i++) {
-    rotors.power(MOTOR_1,255-i);
-    rotors.power(MOTOR_2,255-i);
-    rotors.power(MOTOR_3,255-i);
-    rotors.power(MOTOR_4,255-i);
-    delay(10);
+  if (nDevices == 0){
+    Serial.println("No I2C devices found");
   }
-
-  set_RGB_LED(1,0,0);
-  rotors.stop(MOTOR_1);
-  rotors.stop(MOTOR_2);
-  rotors.stop(MOTOR_3);
-  rotors.stop(MOTOR_4);
-  delay(1000);
-
 }
